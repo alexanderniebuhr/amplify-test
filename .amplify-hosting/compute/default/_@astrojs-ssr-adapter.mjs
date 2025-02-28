b7d6aa77f95027b640ade3b8d9a7779da31b9397
Alexander Niebuhr <alexander@nbhr.io>
2025-02-28 10:54:59 +0100

    first commit

---
+++ .amplify-hosting/compute/default/_@astrojs-ssr-adapter.mjs
@@ -0,0 +1,1 @@
+export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_BPm14-k1.mjs';
---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import amplify from "@craftlions/astro-adapter";
+// https://astro.build/config
+export default defineConfig({
+  adapter: amplify()
+});
