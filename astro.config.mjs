df12562bc27bfbe8057de40bd11f319e65a38167
Alexander Niebuhr <alexander@nbhr.io>
2025-04-24 09:49:44 +0200

    vue-example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import amplify from "@craftlions/astro-adapter";
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-    adapter: amplify()
+  output: 'server',
+  adapter: amplify(),
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
